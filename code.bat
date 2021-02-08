62fb1f5ca609a8ee2bfb152791d2f648d429323a
husnain110 <77391782+husnain110@users.noreply.github.com>
2021-02-08 19:57:11 +0500

    Add files via upload

---
+++ code.bat
@@ -0,0 +1,15 @@
+@echooff Start 
+chrome "" Start 
+chrome "" Start 
+chrome "" Start 
+chrome "" Start 
+chrome "" Start 
+chrome "" Start 
+chrome "" Start 
+chrome "" Start 
+chrome "" Start 
+OUTLOOK start 
+powershell start 
+powershell_ise mstsc 
+start start Start 
+pause
